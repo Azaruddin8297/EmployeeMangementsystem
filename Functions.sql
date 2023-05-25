64ec66e7a23e5333496a69a0973c3b8cfda1d433
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-05-26 00:34:54 +0530

    storedprocedures

--- Functions.sql
+++ Functions.sql
@@ -96,3 +96,72 @@ end
 
 SELECT dbo.NetSales(10,2,1);
 
+create function EmpDetailsTable()
+returns table
+as 
+return(select * from Employee)
+
+select * from EmpDetailsTable()
+
+/*----------------------*/
+
+CREATE TABLE Student (      
+  id int PRIMARY KEY IDENTITY,     
+  admission_no varchar(45) NOT NULL,  
+  first_name varchar(45) NOT NULL,      
+  last_name varchar(45) NOT NULL,  
+  age int,  
+  city varchar(25) NOT NULL      
+)
+  
+CREATE TABLE Fee (   
+  admission_no varchar(45) NOT NULL,  
+  course varchar(45) NOT NULL,      
+  amount_paid int,    
+)
+
+INSERT INTO Student (admission_no, first_name, last_name, age, city)       
+VALUES (3354,'Luisa', 'Evans', 13, 'Texas'),       
+(2135, 'Paul', 'Ward', 15, 'Alaska'),       
+(4321, 'Peter', 'Bennett', 14, 'California'),    
+(4213,'Carlos', 'Patterson', 17, 'New York'),       
+(5112, 'Rose', 'Huges', 16, 'Florida'),  
+(6113, 'Marielia', 'Simmons', 15, 'Arizona'),    
+(7555,'Antonio', 'Butler', 14, 'New York'),       
+(8345, 'Diego', 'Cox', 13, 'California')  
+  
+  
+INSERT INTO Fee (admission_no, course, amount_paid)       
+VALUES (3354,'Java', 20000),       
+(7555, 'Android', 22000),       
+(4321, 'Python', 18000),    
+(8345,'SQL', 15000),       
+(5112, 'Machine Learning', 30000)
+
+SELECT Student.admission_no, Student.first_name, Student.last_name, Fee.course, Fee.amount_paid  
+FROM Student  
+INNER JOIN Fee  
+ON Student.admission_no = Fee.admission_no
+
+create index ix_Fee_amount_paid
+on Fee(amount_paid asc)
+sp_Helpindex Fee
+
+select * from Fee
+
+create procedure spStudent
+as
+begin
+select * from Student where age<18
+end
+spStudent
+
+create procedure spStudent2
+@admission_no varchar(45),
+@age int
+as
+begin
+select id , admission_no,first_name,last_name,age,city from Student
+where admission_no = @admission_no and age = @age
+end
+spStudent2 4213,17
